b567ff8ac8253e74eded7fdedb15cc3b49be7874
Vinícius Santos <vinicius.ssantos.dev@gmail.com>
2022-09-01 00:41:41 -0300

    started creating login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,43 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({ height: '200px' })),
+      state('fade', style({ opacity: 0 })),
+      transition('void => fade', [animate('0.2s ease-in-out')]),
+      transition('void => open', [animate('1s ease-in-out')]),
+    ]),
+  ],
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public isOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      documentNumber: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  public showInput(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  public goShop(): void {
+    console.log('shop');
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
