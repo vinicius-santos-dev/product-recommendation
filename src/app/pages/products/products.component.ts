217e4d913fcca0edba1576f09da1356a365c78ab
Vinícius Santos <vinicius.ssantos.dev@gmail.com>
2022-09-08 00:18:15 -0300

    Started creating products page (sketch)

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  public categories = [
+    { name: 'Todos' },
+    { name: 'Frutas' },
+    { name: 'Vegetais' },
+    { name: 'Lanches' },
+  ];
 
-  constructor() { }
+  // generate 10 products with name and price
+  public products = [
+    { name: 'Banana', price: 'R$ 2,00' },
+    { name: 'Maçã', price: 'R$ 3,00' },
+    { name: 'Pera', price: 'R$ 4,00' },
+    { name: 'Uva', price: 'R$ 5,00' },
+    { name: 'Melancia', price: 'R$ 6,00' },
+    { name: 'Abacaxi', price: 'R$ 7,00' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [ProductsComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MatTabsModule
   ]
 })
 export class ProductsModule { }
